cec82c8493eef47dd54dbc71d9a631c30ee53ecd
deuspoeticus <omerfarukkarasahan@gmail.com>
2024-06-16 03:43:19 +0300

    modified custom disk

--- src/index.tsx
+++ src/index.tsx
@@ -5,14 +5,14 @@ import App from "./App";
 import {canSaveDisks} from "./canSaveDisks";
 
 
-const gradients = [
-      'background-1', 'background-2', 'background-3', 
-      'background-4', 'background-5', 'background-6', 
-      'background-7', 'background-8', 'background-9', 
-      'background-10'
-    ];
-const randomGradient = gradients[Math.floor(Math.random() * gradients.length)];
-document.body.classList.add(randomGradient);
+// const gradients = [
+//       'background-1', 'background-2', 'background-3', 
+//       'background-4', 'background-5', 'background-6', 
+//       'background-7', 'background-8', 'background-9', 
+//       'background-10'
+//     ];
+// const randomGradient = gradients[Math.floor(Math.random() * gradients.length)];
+// document.body.classList.add(randomGradient);
 
 // Determining if we can save data is an async operation, kick off the request
 // now so that we're more likely to have the answer by the time the we actually
