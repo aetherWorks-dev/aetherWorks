b753a9c8319e1582671ce2a32d1aa4e9ebc81ba3
deuspoeticus <omerfarukkarasahan@gmail.com>
2024-06-28 01:49:24 +0300

    netlify header config

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,8 @@ const headers = {
     "Referrer-Policy": "unsafe-url",
     "Feature-Policy": "none",
     // Add Content-Security-Policy header
-    "Content-Security-Policy": "frame-ancestors 'self' https://zora.co https://testnet.zora.co;"
+    "Content-Security-Policy": "frame-ancestors 'self' https://zora.co https://testnet.zora.co",
+    "X-Frame-Options": "ALLOW-FROM https://zora.co https://testnet.zora.co"
   };
 
 export default defineConfig(() => {
